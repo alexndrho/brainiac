ce26dae12d081b0fdbb6597e7f77d099581c1a4f
Alexander Ho <ho.alexander.g@gmail.com>
2023-07-27 19:13:16 +0800

    refactor: removed toggleTheme function

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -1,42 +1,26 @@
-import { useState } from 'react';
-import {
-  ColorScheme,
-  ColorSchemeProvider,
-  MantineProvider,
-} from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
 
 interface ThemeProps {
   children: React.ReactNode;
 }
 
 const Theme = ({ children }: ThemeProps) => {
-  const [colorScheme, setColorScheme] = useState<ColorScheme>('dark');
-
-  const toggleColorScheme = (value?: ColorScheme) => {
-    setColorScheme(value || colorScheme === 'dark' ? 'light' : 'dark');
-  };
-
   return (
-    <ColorSchemeProvider
-      colorScheme={colorScheme}
-      toggleColorScheme={toggleColorScheme}
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: 'dark',
+        globalStyles: () => ({
+          'html, body, #root': {
+            height: '100%',
+            minHeight: '100%',
+          },
+        }),
+      }}
     >
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          colorScheme,
-          globalStyles: () => ({
-            'html, body, #root': {
-              height: '100%',
-              minHeight: '100%',
-            },
-          }),
-        }}
-      >
-        {children}
-      </MantineProvider>
-    </ColorSchemeProvider>
+      {children}
+    </MantineProvider>
   );
 };
 
